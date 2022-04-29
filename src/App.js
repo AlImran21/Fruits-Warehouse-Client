d031d3db3ea8d94263d6ec916323e786c9ca0fd6
Al-Imran <alimran21feb@gmail.com>
2022-04-29 22:37:04 +0600

    added 404page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from '../src/components/Home/Home';
 import Header from './components/Header/Header';
+import NotFound from './components/NotFound/NotFound';
 
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/home' element={<Home />}></Route>
+
+        <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Error404 from '../../images/404.webp';
+import './NotFound.css';
+
+
+const NotFound = () => {
+    return (
+        <div>
+            <div className='error-alignment'>
+               <img src={Error404} alt="" />
+            </div>
+
+        </div>
+    );
+};
+
+export default NotFound;
