47ebd27219f6a8c0f34690df2837b43ce58c5287
Al-Imran <alimran21feb@gmail.com>
2022-04-29 19:48:07 +0600

    added home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from '../src/components/Home/Home';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
       
     </div>
   );
