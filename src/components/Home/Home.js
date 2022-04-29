bfaa209bfbfad69bfc84069af41fdc565ade2911
Al-Imran <alimran21feb@gmail.com>
2022-04-29 19:57:07 +0600

    checked commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,9 +3,8 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>This is home</h1>
-            <h1>This is home</h1>
-            <h1>This is home</h1>
+          <h1>Hello world</h1>
+          <h2>This is home page</h2>
         </div>
     );
 };
