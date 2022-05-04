ec648da8c43bc4fa4a42683b937a8f5ec96761fd
Al-Imran <alimran21feb@gmail.com>
2022-05-04 19:25:48 +0600

    added responsive header and footer only mobile and desktop user

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,31 +16,30 @@ const Header = () => {
 
 
     return (
-        <nav>
-            <div className='header'>
-                <div>
-                    <Link className='d-flex align-items-center logo' to='/'>
-                        <img height={'30px'} src={Logo} alt="" />
-                        <h6 className='ms-1 mt-2'>Fruit's Warehouse</h6>
-                    </Link>
-                </div>
-                <div className='d-flex align-items-center'>
-                    <CustomLink to='/'>Home</CustomLink>
-                    <CustomLink to='/blogs'>Blogs</CustomLink>
-                    {
-                        user &&
-                        <>
-                            <CustomLink to='/addItem'>Add</CustomLink>
-                            <CustomLink to='/manageItems'>Manage</CustomLink>
-                        </>
-                    }
-                    {
-                        user ?
-                            <button onClick={handleSignOut} className='sign-out-button'>Sign Out</button>
-                            :
-                            <CustomLink to='/login'>Login</CustomLink>
-                    }
-                </div>
+        <nav className='header'>
+            <div>
+                <Link className='d-flex align-items-center logo' to='/'>
+                    <img height={'30px'} src={Logo} alt="" />
+                    <h6 className='ms-1 mt-2'>Fruit's Warehouse</h6>
+                </Link>
+            </div>
+            <div className='d-flex align-items-center'>
+                <CustomLink to='/'>Home</CustomLink>
+                {
+                    user &&
+                    <>
+                        <CustomLink to='/addItem'>Add</CustomLink>
+                        <CustomLink to='/manageItems'>Manage</CustomLink>
+                        <CustomLink to='/myItem'>Items</CustomLink>
+                    </>
+                }
+                <CustomLink to='/blogs'>Blogs</CustomLink>
+                {
+                    user ?
+                        <button onClick={handleSignOut} className='sign-out-button'>Sign Out</button>
+                        :
+                        <CustomLink to='/login'>Login</CustomLink>
+                }
             </div>
         </nav>
     );
